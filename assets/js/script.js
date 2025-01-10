30a1663b9eeaca1251019a586af504c9755350b0
Filipa <filipa.ferreirasilva@gmail.com>
2025-01-10 00:28:56 +0000

    Updated folder structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
-fetch('../components/navbar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('navbar').innerHTML = data;
-      });
+fetch("../components/navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar").innerHTML = data;
+  });
 
-    // Include Footer
-    fetch('../components/footer.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('footer').innerHTML = data;
-      });
+// Include Footer
+fetch("../components/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
+
+
+  //To show images when hovering in classes
+
+const items = document.querySelectorAll(".hover-item");
+const background = document.querySelector(".background-image");
+
+items.forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    const imageUrl = item.getAttribute("data-image");
+    background.style.backgroundImage = `url(${imageUrl})`;
+    background.style.opacity = 1;
+  });
+
+  item.addEventListener("mouseleave", () => {
+    background.style.opacity = 0;
+  });
+});
