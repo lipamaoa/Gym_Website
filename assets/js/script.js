23e202ef0c93ecb5bc8f6fccaccef722d056316a
Filipa <filipa.ferreirasilva@gmail.com>
2025-01-20 13:52:10 +0000

    Added toaster

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,18 +20,25 @@ items.forEach((item) => {
   item.addEventListener("mouseenter", () => {
     const imageUrl = item.getAttribute("data-image");
     background.style.backgroundImage = `url(${imageUrl})`;
-    background.style.opacity = 1; // Show the image
+    background.style.opacity = 1; 
   });
 
   item.addEventListener("mouseleave", () => {
-    background.style.opacity = 0; // Hide the image
+    background.style.opacity = 0; 
   });
 });
 
-function showSuccessToast() {
+
+
+//To show toaster with a success message 
+function showSuccessToast(message) {
   const toastElement = document.getElementById("successToast");
-  toastElement.querySelector('.toast-body').textContent=message;
-  const toast=new bootstrap.Toast(toastElement);
-  
-  toast.show();
+
+  if (toastElement) {
+    toastElement.querySelector(".toast-body").textContent = message;
+    const toast = new bootstrap.Toast(toastElement);
+    toast.show();
+  } else {
+    console.error("Elemento com id 'successToast' não encontrado.");
+  }
 }
