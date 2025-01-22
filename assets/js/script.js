c9d481eb82b0e8425865597c029a5020379f4d3a
Filipa <filipa.ferreirasilva@gmail.com>
2025-01-22 19:06:09 +0000

    Some adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,17 +20,15 @@ items.forEach((item) => {
   item.addEventListener("mouseenter", () => {
     const imageUrl = item.getAttribute("data-image");
     background.style.backgroundImage = `url(${imageUrl})`;
-    background.style.opacity = 1; 
+    background.style.opacity = 1;
   });
 
   item.addEventListener("mouseleave", () => {
-    background.style.opacity = 0; 
+    background.style.opacity = 0;
   });
 });
 
-
-
-//To show toaster with a success message 
+//To show toaster with a success message
 function showSuccessToast(message) {
   const toastElement = document.getElementById("successToast");
 
@@ -42,3 +40,5 @@ function showSuccessToast(message) {
     console.error("Elemento com id 'successToast' não encontrado.");
   }
 }
+
+
